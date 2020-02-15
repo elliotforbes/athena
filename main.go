5abb645cdef02b832d9a4ac5479a856a8eb99036
Elliot Forbes <emforce@live.com>
2020-02-15 22:02:24 +0000

    Updates

--- main.go
+++ main.go
@@ -2,26 +2,14 @@ package main
 
 import (
 	"fmt"
-	"net"
-	"strconv"
-	"time"
+
+	"github.com/elliotforbes/athena/port"
 )
-
-func scanPort(protocol, hostname string, port int) bool {
-	address := hostname + ":" + strconv.Itoa(port)
-	conn, err := net.DialTimeout(protocol, address, 60*time.Second)
-
-	if err != nil {
-		return false
-	}
-	defer conn.Close()
-	return true
-}
 
 func main() {
 	fmt.Println("Port Scanning")
-	open := scanPort("tcp", "localhost", 1313)
+	open := port.ScanPort("tcp", "localhost", 1313)
 	fmt.Printf("Port Open: %t\n", open)
-	open = scanPort("tcp", "localhost", 1314)
+	open = port.ScanPort("tcp", "localhost", 1314)
 	fmt.Printf("Port Open: %t\n", open)
 }
---
+++ port/portscanner.go
@@ -0,0 +1,18 @@
+package port
+
+import (
+	"net"
+	"strconv"
+	"time"
+)
+
+func ScanPort(protocol, hostname string, port int) bool {
+	address := hostname + ":" + strconv.Itoa(port)
+	conn, err := net.DialTimeout(protocol, address, 60*time.Second)
+
+	if err != nil {
+		return false
+	}
+	defer conn.Close()
+	return true
+}
